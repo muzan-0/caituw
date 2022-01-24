9d762df13bb35eb9c2bdac63766765814b5b6341
Micah Eaton <80413333+muzan-0@users.noreply.github.com>
2022-01-24 16:29:17 +0000

    Added modular functionality for prefixes

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import os
 import requests
 import errors
 import random
+import re
+
+prefix = '?'
 
 client = discord.Client()
 
+dscregex = re.compile('?')
 
 @client.event
 async def on_ready():
@@ -20,32 +24,32 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('?ping'):
+    if message.content.startswith(f'{prefix}ping'):
         await message.channel.send(
             f'pong dumbass :upside_down: \n**{round(client.latency) * 1000}ms**'
         )
 
-    if message.content.startswith('?insult'):
+    if message.content.startswith(f'{prefix}insult'):
         await message.channel.send(
             requests.get('https://insult.mattbas.org/api/insult').text)
 
-    if message.content.startswith('?bully'):
+    if message.content.startswith(f'{prefix}bully'):
         if message.author.guild_permissions.administrator:
             for i in range(10):
                 await message.channel.send('@everyone')
         else:
             await message.channel.send(errors.NO_PERMISSION)
 
-    if message.content.startswith('?dead'):
+    if message.content.startswith(f'{prefix}dead'):
         await message.channel.send(
             random.choice([
                 'ur ded lol', 'alive, but not for long\n(look behind u)',
                 'ur alive (imagine)', ':skull:'
             ]))
 
-    if message.content.startswith('?cope'):
+    if message.content.startswith(f'{prefix}cope'):
         await message.channel.send('cope')
-    if message.content.startswith('?source'):
+    if message.content.startswith(f'{prefix}source'):
         await message.channel.send(f'''
     Caituw is and always will be open source on github:thumbsup:
 <https://github.com/muzan-0/caituw>
