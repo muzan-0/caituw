1f1e35f318ea913cd45af0c8a7b5a375c75cd8e0
Micah Eaton <80413333+muzan-0@users.noreply.github.com>
2022-01-24 15:49:25 +0000

    Added a way to read the messages being sent without being in discord (Don’t use this branch if you don’t want me to read your messages)

--- main.py
+++ main.py
@@ -4,44 +4,55 @@ import requests
 import errors
 import random
 
-
 client = discord.Client()
-
 
 
 @client.event
 async def on_ready():
-  print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {client.user}\n')
+
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+  
+    print(f'{message.guild}\n#{message.channel}\n@{message.author}\n"{message.content}""\n')
+  
+    if message.author == client.user:
+        return
 
-  if message.content.startswith('?ping'):
-    await message.channel.send(f'pong dumbass :upside_down: \n**{round(client.latency) * 1000}ms**')
+    if message.content.startswith('?ping'):
+        await message.channel.send(
+            f'pong dumbass :upside_down: \n**{round(client.latency) * 1000}ms**'
+        )
 
-  if message.content.startswith('?insult'):
-    await message.channel.send(requests.get('https://insult.mattbas.org/api/insult').text)
+    if message.content.startswith('?insult'):
+        await message.channel.send(
+            requests.get('https://insult.mattbas.org/api/insult').text)
 
-  if message.content.startswith('?bully'):
-    if message.author.guild_permissions.administrator:
-      for i in range(10):
-        await message.channel.send('@everyone')
-    else:
-      await message.channel.send(errors.NO_PERMISSION)
+    if message.content.startswith('?bully'):
+        if message.author.guild_permissions.administrator:
+            for i in range(10):
+                await message.channel.send('@everyone')
+        else:
+            await message.channel.send(errors.NO_PERMISSION)
 
-  if message.content.startswith('?dead'):
-    await message.channel.send(random.choice(['ur ded lol', 'alive, but not for long\n(look behind u)']))
+    if message.content.startswith('?dead'):
+        await message.channel.send(
+            random.choice([
+                'ur ded lol', 'alive, but not for long\n(look behind u)',
+                'ur alive (imagine)', ':skull:'
+            ]))
 
-  if message.content.startswith('?source'):
-    await message.channel.send(f'''
-Caituw is and always will be open source on github:thumbsup:
+    if message.content.startswith('?cope'):
+        await message.channel.send('cope')
+    if message.content.startswith('?source'):
+        await message.channel.send(f'''
+    Caituw is and always will be open source on github:thumbsup:
 <https://github.com/muzan-0/caituw>
 
 Hosted on repl.it
 <https://replit.com/@MicahEaton/caituw?v=1>
     ''')
 
-client.run(os.getenv('TOKEN'))
 
+client.run(os.getenv('TOKEN'))
